57926f67cd24f4da9e7fd30fa796ff25bdbaa143
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-01-21 10:39:15 -0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+// const body = document.querySelector("body");
+// body.removeChild("#main");
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.appendChild(newHeader);
